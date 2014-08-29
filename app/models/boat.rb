19d014263bcfea7d68cd6e4b43618eaa3d64b407
William Harris <wharris@upscalews.com>
2014-08-29 16:10:50 -0700

    made customer_count and availability
    also:
      - updated .gitignore

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,2 +1,12 @@
 class Boat < ActiveRecord::Base
+  has_many :timeslots, through: :assignments
+  #name and capacity come from db
+
+  #a booking group cannot be split across different boats.
+
+  #a boat can only be used for a single timeslot at any given time.
+
+  def amount_booked_by_timeslot(timeslot) 
+    self.assignments.where(timeslot: timeslot).first.booking.size
+  end
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,2 +1,32 @@
 class Timeslot < ActiveRecord::Base
+  has_many :boats, through: :assignments
+  
+  def availability
+  # The availability is the maximum booking size of any new booking on this timeslot. 
+    return 0 if self.assignments.blank? 
+
+    avail = 0
+
+    self.boats.each do |boat|
+      # naive and slow :(
+      
+      cap = boat.capacity
+      amt_booked = boat.amount_booked_by_timeslot(self) 
+      
+      avail_of_boat = cap - amt_booked
+
+      avail = avail_of_boat if avail_of_boat > avail
+    end
+
+    avail
+  end
+
+  def customer_count
+  # The customer count is the total number of customers booked for this timeslot.
+    return 0 if self.boats.blank? or if self.bookings.blank? 
+
+    self.bookings.collect(:size)
+  end
+
+  # boats come from db reference association
 end
