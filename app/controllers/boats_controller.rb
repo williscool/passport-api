41c91e396707e85e788465756c2aeb83369f1c34
William Harris <wharris@upscalews.com>
2014-08-29 22:52:16 -0700

    boats controller and test

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,11 @@
 class BoatsController < ApplicationController
+  def create
+    @boat = Boat.create(capacity: params[:capacity], name: params[:name])   
+    render json: @boat
+  end
+
+  def index
+    @boats = Boat.all
+    render json: @boats
+  end
 end
--- spec/controllers/boats_controller_spec.rb
+++ spec/controllers/boats_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe BoatsController do
+    it "assigns timeslots" do
+      boat = FactoryGirl.create(:boat)
+      get :index
+      expect(assigns(:boats)).to eq([boat])
+    end
 
+    it "assigns timeslot" do
+      post :create, name:"titan", capacity: 8
+      expect(assigns(:boat).name).to eq("titan")
+    end
 end
