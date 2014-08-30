985bfb244140622e0b3da2fd2cd61f3220975a50
William Harris <wharris@upscalews.com>
2014-08-29 22:32:15 -0700

    timeslot controller works
    also:
     - list routes for info

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -1,2 +1,18 @@
 class TimeslotsController < ApplicationController
+  def create
+    @timeslot = Timeslot.create(start_time: params[:start_time], duration: params[:duration])   
+    render json: @timeslot
+  end
+
+  def index
+    
+    if params[:date].present?
+      @day = params[:date].to_date
+      @timeslots = Timeslot.where(start_time: @day.beginning_of_day..@day.end_of_day)
+    else
+      @timeslots = Timeslot.all 
+    end
+
+    render json: @timeslots
+  end
 end
--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe TimeslotsController do
 
+  describe "GET index" do
+    it "assigns timeslots" do
+      ts = FactoryGirl.create(:timeslot)
+      get :index
+      expect(assigns(:timeslots)).to eq([ts])
+    end
+
+    it "works with date param" do
+      date_string = "2014-08-27"
+      time = date_string.to_date + 6.hours
+
+      ts = FactoryGirl.create(:timeslot, start_time: time )
+      get :index, date: date_string
+      expect(assigns(:timeslots)).to eq([ts])
+    end
+  end
+
 end
