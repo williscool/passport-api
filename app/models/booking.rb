3de75ef2c75c0f862e78057619a758e73358dc23
William Harris <wharris@upscalews.com>
2014-08-29 19:41:33 -0700

    update booking model to check size
    make sure its greater than zero but smaller than the largest availability

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,23 @@
 class Booking < ActiveRecord::Base
   # size comes from db
-  #a booking group cannot be split across different boats.
 
-  # you can have multple bookings to a time slot but they cant total more than the capcity of the boat
+  # a booking group cannot be split across different boats.
+
+  # you can have multiple bookings to a time slot
+  # but they cant total more than the capcity of the boats assigned to this booking's timeslot
+  before_validation :booking_size_avalible, on: :create
+
   belongs_to :boat
   belongs_to :timeslot
+
+  def booking_size_avalible
+    if self.size <= 0
+      errors.add(:size, "party size must be 1 or greater")
+    end
+
+    if self.timeslot.availability < self.size
+      errors.add(:size, "this party size is too big for this time slot")
+    end
+  end
+
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Booking do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can only be done when there is enough timeslot availability" do 
+      timeslot = FactoryGirl.create(:timeslot)
+      big_boat = timeslot.boats.create(name:"titan", capacity: 10)
+      small_boat = timeslot.boats.create(name:"lil b", capacity: 6)
+
+      timeslot.bookings.create(boat: big_boat, size:7)
+      booking = timeslot.bookings.create(boat: small_boat, size:22)
+
+      expect(booking).to have(1).errors_on(:size)
+  end
+
+  it "size must be greater than zero" do 
+      timeslot = FactoryGirl.create(:timeslot)
+      small_boat = timeslot.boats.create(name:"titan", capacity: 10)
+
+      booking_neg = timeslot.bookings.create(boat: small_boat, size:-1)
+      booking_zero = timeslot.bookings.create(boat: small_boat, size:0)
+
+      expect(booking_neg).to have(1).errors_on(:size)
+      expect(booking_zero).to have(1).errors_on(:size)
+  end
 end
