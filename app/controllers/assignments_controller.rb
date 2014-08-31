15fc730a14d0ff62217230db51bb234c7c3ce2c1
William Harris <wharris@upscalews.com>
2014-08-30 19:35:34 -0700

    assignments controller and test

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,2 +1,11 @@
 class AssignmentsController < ApplicationController
+  def create
+    @assignment = Assignment.create(timeslot: Timeslot.find(params[:timeslot_id]), boat: Boat.find(params[:boat_id]))   
+    render json: @assignment
+  end
+
+  def index
+    @assignments = Assignment.all
+    render json: @assignments
+  end
 end
--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -2,4 +2,29 @@ require 'spec_helper'
 
 describe AssignmentsController do
 
+  describe "GET index" do
+    it "assigns assignments" do
+      timeslot = FactoryGirl.create(:timeslot, duration: 30, start_time: Time.now + 3.hours)
+      big_boat = timeslot.boats.create(name:"titan", capacity: 10)
+
+      timeslot.assignments.create(boat: big_boat)
+
+      get :index
+      expect(assigns(:assignments)).to eq([timeslot.assignments.first])
+    end
+  end
+
+  describe "POST create" do
+    it "assigns boat to timeslot" do
+      timeslot = FactoryGirl.create(:timeslot, duration: 30, start_time: Time.now + 3.hours)
+      big_boat = timeslot.boats.create(name:"titan", capacity: 10)
+
+      ts_id = timeslot.id
+      b_id = big_boat.id
+
+      post :create, timeslot_id: ts_id, boat_id: b_id
+
+      expect((timeslot.assignments.first).boat).to eq(big_boat)
+    end
+  end
 end
